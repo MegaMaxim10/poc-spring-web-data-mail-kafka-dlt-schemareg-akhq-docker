e4fd240c5b08e856129b501b9465b510a58fbd0b
Zekeng Ndadji Milliam Maxime <ndadjimaxime@yahoo.fr>
2021-08-12 20:49:03 +0100

    Added mailslupper in docker-compose

--- deployment/docker/run.sh
+++ deployment/docker/run.sh
@@ -1,6 +1,1 @@
 sudo docker-compose up -d --build
-
-sudo docker run \
-    -v $PWD/mailslurper-config.json:/opt/mailslurper/config.json \
-    -p 2500:2500 -p 8010:8010 -p 8015:8015 \
-    marcopas/docker-mailslurper
