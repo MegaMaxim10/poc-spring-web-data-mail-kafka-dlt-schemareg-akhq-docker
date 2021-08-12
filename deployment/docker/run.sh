34689169a1c0cdb91f3b8d441c89b287e9efd43b
Zekeng Ndadji Milliam Maxime <ndadjimaxime@yahoo.fr>
2021-08-12 19:49:57 +0100

    Added mailslupper, akhq and many other tools

--- deployment/docker/run.sh
+++ deployment/docker/run.sh
@@ -1,11 +1,6 @@
-#!/bin/sh
-
-# ATTENTION, CETTE INSTRUCTION VA STOPPER TOUS LES CONTAINERS
-# sudo docker kill $(sudo docker ps -q)
-
-#cd ./custom-images/activemq
-#sudo docker build -t dimsoft/activemq:latest .
-#sudo docker run -d -p 8161:8161 -p 61616:61616 -p 61613:61613 -p 5672:5672 -p 1883:1883 -p 61614:61614 dimsoft/activemq:latest
-
-#cd ../..
 sudo docker-compose up -d --build
+
+sudo docker run \
+    -v $PWD/mailslurper-config.json:/opt/mailslurper/config.json \
+    -p 2500:2500 -p 8010:8010 -p 8015:8015 \
+    marcopas/docker-mailslurper
